55eb75fac6c9955fbddf37afb02d4f90b210d17d
nanang <nanangaditya2001@gmail.com>
2024-03-28 23:10:01 +0700

    Atur Responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// const navbar = document.getElementById("navbar");
+
+// window.onscroll = function () {
+//   const header = document.querySelector("header");
+//   const fixedNav = header.offsetTop;
+//   const toTop = document.querySelector("#to-top");
+
+//   if (window.pageYOffset > fixedNav) {
+//     header.classList.add("navbar-fixed");
+//     header.classList.add("navbar-blur");
+//     toTop.classList.remove("hidden");
+//     toTop.classList.add("flex");
+//   } else {
+//     header.classList.remove("navbar-fixed");
+//     header.classList.remove("navbar-blur");
+//     toTop.classList.remove("flex");
+//     toTop.classList.add("hidden");
+//   }
+// };
+
 const navbar = document.getElementById("navbar");
 
 window.onscroll = function () {
@@ -7,7 +27,7 @@ window.onscroll = function () {
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
-    header.classList.add("navbar-blur");
+    header.classList.add("navbar-blur"); // Hapus class navbar-blur
     toTop.classList.remove("hidden");
     toTop.classList.add("flex");
   } else {
@@ -18,25 +38,47 @@ window.onscroll = function () {
   }
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+  var offcanvasElementList = [].slice.call(
+    document.querySelectorAll(".offcanvas")
+  );
+  var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+    return new bootstrap.Offcanvas(offcanvasEl);
+  });
+
+  window.addEventListener("scroll", function () {
+    if (offcanvasList[0]._backdrop) {
+      offcanvasList[0]._backdrop.classList.add("show");
+    }
+  });
+});
+
 // owlCarousel
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
     center: true,
     loop: true,
-    margin: 0,
-    // nav: true,
+    margin: 10,
+    nav: false,
+    autoplay: false,
+    paginationSpeed: 500,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    dots: false,
     responsive: {
       0: {
-        items: 0,
+        items: 1,
       },
       600: {
         items: 2,
       },
       1000: {
-        items: 2.3,
+        items: 2,
       },
     },
   });
+
   $(".owl-prev").click(function () {
     $(".owl-carousel").trigger("prev.owl.carousel");
     changeButtonColor(this);
